c753ddde415f845e14a973f09a58b1d2e010fb4e
Kamisia <kamila.samczuk@gmail.com>
2024-04-28 17:25:38 +0200

    style correction

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,14 +4,14 @@ import ButtonCVDownload from "./ButtonCVDownload";
 const Contact = () => {
   return (
     <section className="bg-main p-2 text-white flex flex-col " id="contact">
-      <div className="flex flex-col md:flex-row justify-between m-5">
-        <div className="md:w-1/2">
+      <div className="flex flex-col md:flex-row justify-between m-5 ">
+        <div className="md:w-1/2 ">
           <h1 className="text-4xl font-small mb-2 tracking-wider uppercase">
             Contact
           </h1>
-          <p className="mb-2">kamila@samcz.uk</p>
+          <p className="mb-2 p-2">kamila@samcz.uk</p>
 
-          <div className="flex gap-x-4 mt-4">
+          <div className="flex gap-x-4 mt-4 p-2">
             <a
               href="https://github.com/Kamisia"
               target="_blank"
