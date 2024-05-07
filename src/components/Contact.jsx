38290496af6366ee44b76c7601f97711eb5af747
Kamisia <kamila.samczuk@gmail.com>
2024-05-07 19:53:05 +0200

    changed bg color

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import SocialButtonComponent from "./SocialButtonComponent";
 
 const Contact = () => {
   return (
-    <section className="bg-main p-2 text-white flex flex-col " id="contact">
+    <section className="bg-darkMain p-2 text-white flex flex-col " id="contact">
       <div className="flex flex-col md:flex-row justify-between m-5 ">
         <div className="md:w-1/2 ">
           <h1 className="text-4xl font-small mb-2 tracking-wider uppercase">
