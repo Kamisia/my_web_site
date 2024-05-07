95e7f55873341c02ba61db3e0dd21f25f812b54b
Kamisia <kamila.samczuk@gmail.com>
2024-05-07 20:05:08 +0200

    added link to the project website - project screenshot

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,14 +1,17 @@
-import { FaGithubSquare } from "react-icons/fa";
-import { TbWorldWww } from "react-icons/tb";
+import { FaGithub } from "react-icons/fa";
+
+import { TfiWorld } from "react-icons/tfi";
 
 const ProjectsCard = ({ url, img, github, title, text }) => {
   return (
     <article className="bg-white rounded-lg shadow-md hover:shadow-xl duration-300">
-      <img
-        src={img}
-        alt={title}
-        className="w-full object-cover rounded-t-lg h-64"
-      />
+      <a href={url}>
+        <img
+          src={img}
+          alt={title}
+          className="w-full object-cover rounded-t-lg h-64"
+        />
+      </a>
       <div className=" p-4 h-50">
         <h2 className="text-xl  h-12 tracking-wide font-medium capitalize">
           {title}
@@ -19,10 +22,10 @@ const ProjectsCard = ({ url, img, github, title, text }) => {
 
         <div className="mt-4 flex gap-x-4">
           <a href={url} target="_blank" rel="noopener noreferrer">
-            <TbWorldWww className="h-8 w-8 text-lightAccent hover:text-main duration-300" />
+            <TfiWorld className="h-8 w-8 text-lightAccent hover:text-accent duration-300" />
           </a>
           <a href={github} target="_blank" rel="noopener noreferrer">
-            <FaGithubSquare className="h-8 w-8 text-lightAccent hover:text-main duration-300" />
+            <FaGithub className="h-8 w-8 text-lightAccent hover:text-accent duration-300" />
           </a>
         </div>
       </div>
