a6de7b8e7409715b1de676dacdfc3942830ae015
Kamisia <kamila.samczuk@gmail.com>
2024-09-16 10:30:09 +0200

    About description update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,37 +6,44 @@ const About = () => {
       <SectionTitle text="About" />
       <div className="align-element grid lg:grid-cols-2 items-center gap-16 p-2">
         <article>
-          <p className="text-gray mt-5 leading-loose p-5 text-justify">
-            I am passionate about crafting and&nbsp;personal development,
-            with&nbsp;a&nbsp;deep love for&nbsp;nature. My&nbsp;hobby,
-            experimenting with various crafting techniques, enriches
-            my&nbsp;approach to&nbsp;life and&nbsp;work. For the past two years,
-            I've&nbsp;been fascinated by the art of&nbsp;macramé, which offers
-            me many possibilities for&nbsp;creating both practical
-            and&nbsp;decorative items. I&nbsp;particularly value weaving hanging
-            planters, which adorn my home and plant collection.
-            <br />
-            <br />
-            Regardless of my passions, I&nbsp;always strive for personal
-            and&nbsp;professional growth. Lately, I've been focusing
-            on&nbsp;transitioning from a molecular biotechnologist
-            to&nbsp;a&nbsp;front-end developer. Since my college days, I've been
-            exploring this field, learning from online courses and working
-            on&nbsp;my&nbsp;own projects. My portfolio showcases my latest
-            projects based on React.js, which is my primary interest
-            in&nbsp;front-end development.
-            <br />
-            <br />
-            In the long run, I aim to&nbsp;deepen my knowledge
-            of&nbsp;PostgreSQL and&nbsp;broaden my skills as a full-stack
-            developer.
-            <br />
-            <br />
-            Currently, I am seeking inspiring projects and&nbsp;teams where
-            I&nbsp;can develop, share knowledge, and&nbsp;collaborate.
-            If&nbsp;you're interested in&nbsp;working together
-            or&nbsp;discussing opportunities, feel free to&nbsp;reach out!
-          </p>
+          <div className="text-gray mt-5 leading-loose p-5 text-justify">
+            <p>
+              I am passionate about
+              <span className="font-bold">
+                creation and personal development
+              </span>
+              , with a deep love for nature. My hobby of experimenting with
+              various crafting techniques enriches both my life and work. Over
+              the past two years, I’ve become captivated by macramé, which has
+              allowed me to create practical and decorative items. I
+              particularly enjoy weaving hanging planters that beautifully
+              complement my home and plant collection.
+            </p>
+            <p>
+              Beyond my creative pursuits, I am committed to{" "}
+              <span className="font-bold">
+                continuous personal and professional growth
+              </span>
+              . Recently, I’ve been transitioning from molecular biotechnologist
+              to front-end developer, dedicating significant time to learning
+              through online courses and hands-on projects. My portfolio
+              features my latest work with React.js, reflecting my strong
+              interest in front-end development.
+            </p>
+            <p>
+              I specialize in <span className="font-bold">React.js</span>,{" "}
+              <span className="font-bold">JavaScript</span>, and{" "}
+              <span className="font-bold">JavaScript</span>,{" "}
+              <span className="font-bold">Tailwind CSS</span>, with expertise in{" "}
+              <span className="font-bold">UI component development</span>,{" "}
+              <span className="font-bold">API integration</span>, and{" "}
+              <span className="font-bold">application optimization</span>.
+              Currently, I am expanding my backend skills by learning{" "}
+              <span className="font-bold">Spring</span>. I’m excited to bring my
+              skills and passion to innovative projects.
+            </p>
+            <p>Feel free to reach out to discuss potential collaboration</p>
+          </div>
         </article>
         <img src={aboutSvg} className="w-full h-64 hidden lg:block" />
       </div>
